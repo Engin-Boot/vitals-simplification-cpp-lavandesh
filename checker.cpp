f3009c1b7784605bb93dfbb06092c30a9f024755
lavandesh <31106149+lavandesh@users.noreply.github.com>
2020-08-31 12:22:55 +0530

    Initial commit without test cases

--- checker.cpp
+++ checker.cpp
@@ -1,36 +1,105 @@
 #include <assert.h>
-const int bpmLimits[] = {70, 150};
-const int respRateLimits[] = {30, 95};
-const int spo2Limit = 90;
+#include <iostream>
+using namespace std;
 
-//for vital with upper and lower value
-bool anyVitalIsOk(float value, int low, int high){
-  return (value>low && value<high);
+// Static Information (i.e. names, upper and lower values) of Vitals stored in the system
+typedef struct vitalInfo
+{
+  char* vitalName;
+  int high;
+  int low;
+};
+
+vitalInfo spo2 = {"Blood oxygen", 90, 101};
+vitalInfo bpm = {"BPM", 70, 150};
+vitalInfo respRate = {"Respiratory rate", 30, 95};
+
+struct vitalInfo infoArray[3] = {spo2, bpm, respRate};
+
+//Alert type definations
+class AlertType
+{
+    public:
+    virtual void sendAlert(const char* vitalName, const char* message) = 0;
+
+};
+
+class AlertInSMS : AlertType
+{
+  public:
+  void sendAlert(const char* vitalName, const char* message){
+    std::cout << "SMS sent: " << vitalName << " " << message << std::endl;
+  }
+};
+
+class AlertInAlarm : AlertType
+{
+  public:
+    void sendAlert(const char* vitalName, const char* message){
+      std::cout << "Alarm Sounded: " << vitalName << " " << message << std::endl;
+  }
+};
+
+
+
+//Vital Checker - Checks each vital against safe limits
+bool vitalIsOk(AlertType *alert, char* vitalname, float value, int low, int high){
+  if (value<low)
+  {
+    alert->sendAlert(vitalname, "is low");
+  }
+
+  else if (value>high)
+  {
+    alert->sendAlert(vitalname, "is high");
+  }
+
+  else
+    return true;
 }
-
-//for vital with only cutoff
-bool anyVitalIsOk(float value, int cutOff){
-  return value>=cutOff;
-}
-
   
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return ( anyVitalIsOk(bpm, bpmLimits[0], bpmLimits[1]) && anyVitalIsOk(spo2, spo2Limit) && anyVitalIsOk(respRate, respRateLimits[0], respRateLimits[1]));
+
+//Format of incoming Data
+struct patientVitals{
+  AlertType* alert;
+  vitalInfo vital;
+  float value;  
+};
+
+//Go through the list, populate upper and lower value, and send for check
+bool patientIsOk (patientVitals* vitalList)
+{
+  bool status = true;
+  for(int i = 0 ; i<sizeof(vitalList); i++)
+  {
+    if(!vitalIsOk(vitalList[i].alert, vitalList[i].vital.vitalName, vitalList[i].value, vitalList[i].vital.low, vitalList[i].vital.high))
+    {
+      status = false;
+    }
+  }
+  return status;
 }
+
 
 int main() {
-  assert(vitalsAreOk(80, 95, 60) == true);
-  assert(vitalsAreOk(60, 90, 40) == false);
+
+  AlertInSMS sms;
+  AlertInAlarm alarm;
+
+  //struct patientVitals patient1[3] ={ {&sms, bpm, 80}, }
+  //assert(patientIsOk() == true);
+  //assert(vitalsAreOk(60, 90, 40) == false);
   
-  assert(anyVitalIsOk(10, 5, 15) == true); 
-  assert(anyVitalIsOk(2, 5, 15) == false); 
-  assert(anyVitalIsOk(20, 5, 15) == false); 
+  /*assert(anyVitalIsOk("vital", 10, 5, 15) == true); 
+  assert(anyVitalIsOk("vital", 2, 5, 15) == false); 
+  assert(anyVitalIsOk("vital", 20, 5, 15) == false); 
   
-  assert(anyVitalIsOk(10,20)==false);
-  assert(anyVitalIsOk(30,20)==true);
+  assert(anyVitalIsOk("vital", 10,20, 1000)==false);
+  assert(anyVitalIsOk("vital", 30,20, 1000)==true);
   
-  assert(anyVitalIsOk(10, 50, 100) == false);
-  assert(anyVitalIsOk(150, 50, 100) == false);
-  assert(anyVitalIsOk(75, 50, 100) == true);
+  assert(anyVitalIsOk("vital", 10, 50, 100) == false);
+  assert(anyVitalIsOk("vital", 150, 50, 100) == false);
+  assert(anyVitalIsOk("vital", 75, 50, 100) == true);
+  */
   
 }
